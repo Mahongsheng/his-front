6f72d547c55ac456016c3ad05b02aecd50f500ef
Canis <1025221390@qq.com>
2020-05-08 14:36:55 +0800

    fix some conflicts

--- src/router/index.js
+++ src/router/index.js
@@ -12,14 +12,9 @@ import ReturnDrug from "../components/nav_pharmacy/ReturnDrug";
 import Inspection from "../components/nav_technician/Inspection";
 import Test from "../components/nav_technician/Test";
 import Disposal from "../components/nav_technician/Disposal";
-<<<<<<< HEAD
-import Settlement from "../components/nav_registration/Settlement";
-import Settlement_history from "../components/nav_registration/Settlement_history";
-=======
 import Settlement from "../components/nav_registration/Settlement.vue";
 import Settlement_history from "../components/nav_registration/Settlement_history.vue";
 import FareCheck from "../components/nav_registration/FareCheck"
->>>>>>> 9b39d86c910b43dd7135751a25c747fdaa556260
 
 Vue.use(Router);
 
@@ -128,8 +123,6 @@ export default new Router({
              title:"HIS东软云医院"
            }
          },
-<<<<<<< HEAD
-=======
         {
           path: '/home-admin/fareCheck',
           component: FareCheck,
@@ -139,7 +132,6 @@ export default new Router({
         },
 
 
->>>>>>> 9b39d86c910b43dd7135751a25c747fdaa556260
       ]
     }
   ]
